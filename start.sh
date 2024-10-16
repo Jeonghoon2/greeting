6ab131741a8fe57566933cb70643f356b549c394
Jeonghoon2 <ukidd12@naver.com>
2024-10-16 18:35:08 +0900

    fix sh

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-JAR_FILE_NAME=greeting-0.0.1-SNAPSHOT.jar
+JAR_FILE_NAME=greeting-0.1.0-SNAPSHOT.jar
+JAR_PATH=build/libs/
 
-# 현재 실행 중인 pid 가져오기
 PID=$(pgrep -f $JAR_FILE_NAME)
 
-# 현재 실행중인 process 죽이기
 if [ -z "$PID" ]; then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
@@ -15,8 +14,7 @@ else
   sleep 10
 fi
 
-#다시 시작
+
 echo "> 애플리케이션을 다시 시작합니다..."
-nohup java \
-    -jar build/libs/$JAR_FILE_NAME &
+nohup java -jar $JAR_PATH/$JAR_FILE_NAME > /dev/null 2>&1 &
 echo "> 애플리케이션이 백그라운드에서 실행 중입니다."

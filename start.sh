16e24d8e563cee45742e6f79fa7c69aa8c9f6966
Jeonghoon2 <ukidd12@naver.com>
2024-10-21 10:33:33 +0900

    fix shell

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 JAR_FILE_NAME=greeting-0.0.1-SNAPSHOT.jar
-JAR_PATH=build/libs  # 실제 JAR 파일 경로로 변경
+# JAR_PATH=build/libs  # 실제 JAR 파일 경로로 변경
 
 # 실행 중인 애플리케이션의 PID 가져오기
 PID=$(pgrep -f $JAR_FILE_NAME)
@@ -17,5 +17,6 @@ fi
 
 # nohup을 사용하여 애플리케이션을 백그라운드에서 실행
 echo "> 애플리케이션을 다시 시작합니다..."
-nohup java -jar $JAR_PATH/$JAR_FILE_NAME &
+#nohup java -jar $JAR_PATH/$JAR_FILE_NAME &
+nohup java -jar $JAR_FILE_NAME &
 echo "> 애플리케이션이 백그라운드에서 실행 중입니다."
